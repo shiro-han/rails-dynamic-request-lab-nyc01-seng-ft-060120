af239e6c520ea8189ca1b7c88c4a2a83832e5a5d
shiro-han <shan99business@gmail.com>
2020-06-23 19:59:04 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'Shiro', last_name: 'Han')
+Student.create(first_name: 'Fox', last_name: 'McCloud')
+Student.create(first_name: 'Falco', last_name: 'Lombardi')
+Student.create(first_name: 'Marth', last_name: 'FE')
+Student.create(first_name: 'Jiggly', last_name: 'Puff')
